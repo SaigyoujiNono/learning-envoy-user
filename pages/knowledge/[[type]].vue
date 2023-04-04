c4833f3ca99bc25652e3b2c46f7066e6eb2a9f98
qiaode.mo <qiaode.mo@sfygroup.com>
2023-04-04 17:59:36 +0800

    tag写了一部分

--- pages/knowledge/[[type]].vue
+++ pages/knowledge/[[type]].vue
@@ -4,31 +4,9 @@
       <div class="category-container">
         <v-row :gutters="20">
           <v-col cols="9">
-            <div>课程类别</div>
+            <div class="mb10">课程类别</div>
             <div>
-              <v-row :gutters="40">
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-                <v-col cols="2">
-                  <span>考研</span>
-                </v-col>
-              </v-row>
+              <span v-for="tg in searchTags" :class="{ active: params.type === tg.value }" class="search-tag" :key="tg.value" @click="handleClickTag(tg)">{{ tg.label }}</span>
             </div>
           </v-col>
           <v-col cols="3">
@@ -48,12 +26,27 @@
 </template>
 
 <script setup lang="ts">
+import { DictModel } from '~~/type/BaseModel';
+import { KNOWLEDGE_PAGE } from '~~/constant/address';
+
 useHead({
   title: '学习使者-知识库',
   meta: [
     { name: 'description', content: '学习使者,知识库' }
   ]
 })
+const searchTags = ref<Array<DictModel>>([
+  { value: 'graduate', label: '考研' }, 
+  { value: 'it', label: 'IT' }, 
+  { value: 'Java', label: 'Java' }, 
+  { value: 'Python', label: 'Python' }, 
+  ])
+  const { params } = useRoute()
+  const { resolve, push } = useRouter()
+  // 跳转
+  const handleClickTag = (tg: DictModel) => {
+    push(KNOWLEDGE_PAGE + '/' + tg.value)
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -62,6 +55,25 @@ useHead({
     height: 100%;
     display: flex;
     align-items: center;
+  }
+
+  .search-tag {
+    margin-right: 20px;
+    padding: {
+      left: 12px;
+      right: 12px;
+      top: 4px;
+      bottom: 4px;
+    };
+    cursor: pointer;
+    transition: all 0.3s;
+    border-radius: 5px;
+    &:hover {
+      background-color: aquamarine;
+    }
+    &.active {
+      background-color: aquamarine;
+    }
   }
 }
 </style>
---
+++ type/BaseModel.ts
@@ -0,0 +1,4 @@
+export interface DictModel {
+  value: string
+  label: string
+}
